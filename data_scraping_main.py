e971344caf04b9c46001f9b9c95cdfdad3c4570a
Omerdan <Omerdan03@gmail.com>
2020-10-31 20:13:46 +0200

    Introducing BeautifulSoup

--- data_scraping_main.py
+++ data_scraping_main.py
@@ -4,13 +4,16 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_urls() -> list:
+STOCK_URLS = "data_urls.csv"
+
+
+def get_urls(url_input) -> list:
     """
     # This function read the url_index file and return a list of tuples with the stock name and url
     :return: list for the stocks name and urls [(stock1, stock1_url), (stock1, stock1_url), ...]
     """
     stock_index = []
-    with open('data_urls.csv', newline='') as csvfile:
+    with open(url_input, newline='') as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(1024))
         csvfile.seek(0)
         reader = csv.reader(csvfile, dialect)
@@ -19,25 +22,38 @@ def get_urls() -> list:
     return stock_index[1:]
 
 
-def download_page(url: str) -> str:
+def make_soup(url: str) -> BeautifulSoup:
     """
-    # This function receives an url address and returns a string with the html file
-    :param url: string with an url address
-    :return: string with the list
+    This function gets a url of stocks and returns BeautifulSoup object of that web
+    :param url: a url of a web page
+    :return: BeautifulSoup Object of the file
     """
-    response = urllib.request.urlopen(url)
-    web_content = response.read()
-    return web_content
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+    return soup
+
+
+def get_site_info(soup: BeautifulSoup) -> list:
+    """
+    This function gets a BeautifulSoup Object and return the useful information from that file
+    :param soup:
+    :return:
+    """
+    data_table = soup.find('table')
+
+    print(data_table)
 
 
 def main():
-    urls = get_urls()
-    soups = []
+    urls = get_urls(STOCK_URLS)
+    soups_list = {}
     for url in urls:
-        print("Making {} soup".format(url[0]))
-        html = download_page(url[1])
-        soup = BeautifulSoup(html, 'lxml')
-        soups.append((url[0], soup))
+        stock = url[0]
+        print("Making {} soup".format(stock))
+        soup = make_soup(url[1])
+        soups_list[stock] = soup
+        info = get_site_info(soup)
+
 
 
 
