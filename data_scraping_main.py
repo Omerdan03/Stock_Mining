ad1c08de735644353a75c1c47a4cf89f150007aa
Omerdan <Omerdan03@gmail.com>
2020-11-04 09:05:05 +0200

    background + display year

--- data_scraping_main.py
+++ data_scraping_main.py
@@ -1,5 +1,4 @@
 import csv
-import requests
 from bs4 import BeautifulSoup
 import datetime
 from selenium import webdriver
@@ -7,6 +6,7 @@ import time
 
 STOCK_URLS = "data_urls.csv"
 HEADERS = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+MAX_SCROLLING = 40
 
 
 def get_urls(url_input) -> list:
@@ -15,28 +15,30 @@ def get_urls(url_input) -> list:
     :return: list for the stocks name and urls [(stock1, stock1_url), (stock1, stock1_url), ...]
     """
     stock_index = []
-    with open(url_input, newline='') as csvfile:
-        dialect = csv.Sniffer().sniff(csvfile.read(1024))
-        csvfile.seek(0)
-        reader = csv.reader(csvfile, dialect)
+    with open(url_input, newline='') as file:
+        dialect = csv.Sniffer().sniff(file.read(1024))
+        file.seek(0)
+        reader = csv.reader(file, dialect)
         for row in reader:
             stock_index.append((row[0], row[1]))
     return stock_index[1:]
 
-def make_soup(url: str) -> BeautifulSoup:
+
+def make_soup(url: str, stock_name) -> BeautifulSoup:
     """
     This function gets a url of stocks and returns BeautifulSoup object of that web
     :param url: a url of a web page
+    :param stock_name: The name of the current stock
     :return: BeautifulSoup Object of the file
     """
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
-    scroll_pause_time = 10
     last_height = driver.execute_script("return document.documentElement.scrollHeight")
     count = 0
     while True:
         driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-
         new_height = driver.execute_script("return document.documentElement.scrollHeight")
         if new_height == last_height:
             time.sleep(1)
@@ -44,12 +46,31 @@ def make_soup(url: str) -> BeautifulSoup:
             if new_height == last_height:
                 break
         last_height = new_height
+        if count % 1 == 0:
+            year = get_year(BeautifulSoup(driver.page_source, "html.parser"))
+            print("Reached {} of {} data".format(year, stock_name))
+
         count += 1
-        if count % 20 == 0:
-            print(count)
     soup = BeautifulSoup(driver.page_source, "html.parser")
     driver.close()
     return soup
+
+
+def get_year(soup: BeautifulSoup) -> int:
+    """
+    This function gets a soup object from Yahoo page and return the last year it has data inside it
+    :param soup: A soup object from Yahoo page
+    :return:
+    """
+    data_table = soup.find('table').find('tbody')
+    row = data_table.find_all('tr')[-1]
+    date_item = row.find('span')
+    date = date_str_to_datetime(date_item.text)
+    return date.year
+
+
+
+
 
 def date_str_to_datetime(date_str: str) -> datetime:
     """
@@ -84,10 +105,10 @@ def get_site_info(soup: BeautifulSoup) -> list:
 def main():
     urls = get_urls(STOCK_URLS)
     soups_list = {}
-    for url in urls[0:1]:
+    for url in urls:
         stock = url[0]
         print("Making {} soup".format(stock))
-        soup = make_soup(url[1])
+        soup = make_soup(url[1], stock)
         soups_list[stock] = soup
         info = get_site_info(soup)
         print(info)
