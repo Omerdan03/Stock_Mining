03697f6fbb658441b5073b112b429d2041f04bee
Omerdan <Omerdan03@gmail.com>
2020-11-25 19:18:44 +0200

    Adding config.py file (sql connection setting)

---
+++ config.py
@@ -0,0 +1,8 @@
+class sql_conf:
+    host = 'localhost'
+    user = 'root'
+    password = ''
+
+    @staticmethod
+    def get_connection_info():
+        return sql_conf.host, sql_conf.user, sql_conf.password
