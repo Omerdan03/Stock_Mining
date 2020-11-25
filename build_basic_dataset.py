f5b36b73b9e4b933caaca09f5b0fc64c62ce1e9b
Omerdan <Omerdan03@gmail.com>
2020-11-25 20:39:55 +0200

    Putting the urls in mySQL database

--- build_basic_dataset.py
+++ build_basic_dataset.py
@@ -1,71 +1,68 @@
-import csv
-from selenium.webdriver.common.keys import Keys
 from scraping_tools import *
 from selenium import webdriver
+import mysql.connector as connector
 
 MAIN_URL = "https://finance.yahoo.com"
-HISTORICAL_DATA = 3
 
 
 def load_all_slider_stocks(browser):
+    """
+
+    :param browser:
+    :return:
+    """
     financial_header = browser.find_element_by_id('Lead-3-FinanceHeader-Proxy')
     next_stock_button = financial_header.find_elements_by_tag_name('button')[1]
     while (next_stock_button.is_enabled()):
         next_stock_button.click()
 
 
-def get_urls(yahoo_main) -> list:
+def get_full_page(yahoo_main) -> list:
     """
-    # This function receives and main url of yahoo finance and return a list with the urls for the top indexes
-    :param yahoo_main: The url for yahoo finance home web page
-    :return: list for the stocks name and urls [(stock1, stock1_url), (stock1, stock1_url), ...]
+    This function receives the main url og yahoo finance and returns a list with BeautifulSoup objects with the items
+     inside the top slider
+    :param yahoo_main:
+    :return:
     """
-
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     browser = webdriver.Chrome(options=chrome_options)
     browser.get(yahoo_main)
     load_all_slider_stocks(browser)
     main_soup = BeautifulSoup(browser.page_source, features="lxml")
-    urls = []
-
-    slider = main_soup.find('div', {'id': 'YDC-Lead'})  # TODO expend the slider to show all indexes
+    slider = main_soup.find('div', {'id': 'YDC-Lead'})
     stocks = slider.find_all('li')
-    for i, stock in enumerate(stocks):
-        name = stock.attrs['aria-label']
-        print("making index of {}\n {} out of {}".format(name, i+1, len(stocks)))
-        url = yahoo_main + stock.find('a').attrs['href']
-        stock_soup = make_soup(url, scrolling=False)
-        opt = stock_soup.find('div', {'id': 'quote-nav'})
-        for tab in opt.find_all('li'):
-            if tab.find('span').getText() == 'Historical Data':
-                full_history_url = yahoo_main + tab.find('a').attrs['href'].split('p=')[
-                    0] + 'period1=-3000000000&period2=3000000000&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true'
-                break
-        # browser.get(full_history_url)
-        # stock_history = get_all_historical_stock_data(browser) #do something with stock history?
-        urls.append([name, full_history_url])
-
-    return urls
-
-
-
+    return stocks
 
 
 def main():
-    # get the names of stocks in the main page
-    # go over each page, load it, and parse it
-    stocks_urls = get_urls(MAIN_URL)
-    with open('data_urls.csv', 'w', newline='') as csv_file:
-        stocks = ['Stock_name', 'Yahoo_url']
-        writer = csv.DictWriter(csv_file, fieldnames=stocks)
-        writer.writeheader()
-        for stock in stocks_urls:
-            writer.writerow({"Stock_name": stock[0],
-                             'Yahoo_url': stock[1]})
+    yahoo_main = MAIN_URL
+    con = connect_to_mysql()
+    cursor = con.cursor()
+    cursor.execute(f"USE stocks_db;")
+    stocks = get_full_page(yahoo_main)
+    for i, stock in enumerate(stocks):
+        name = stock.attrs['aria-label']
+        print("writing index of {}\n {} out of {}".format(name, i + 1, len(stocks)))
+        url = yahoo_main + stock.find('a').attrs['href']
+        stock_soup = make_soup(url)
+        opt = stock_soup.find('div', {'id': 'quote-nav'})
+        for tab in opt.find_all('li'):
+            if tab.find('span').getText() == 'Historical Data':
+                full_history_url = yahoo_main + tab.find('a').attrs['href'].split('p=')[0] + \
+                                   'period1=-3000000000&period2=3000000000&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true'
+                break
+        # browser.get(full_history_url)
+        # stock_history = get_all_historical_stock_data(browser) #do something with stock history?
+        add_query = f"INSERT INTO stock_info (stock_name, url) VALUES('{name}', '{full_history_url}');"
+        try:
+            cursor.execute(add_query)
+        except connector.errors.IntegrityError:
+            del_query = f"DELETE FROM stocks_db.stock_info WHERE stock_name = '{name}';"
+            cursor.execute(del_query)
+            cursor.execute(add_query)
+        con.commit()
 
 
 if __name__ == '__main__':
     main()
-
-#  TODO Error handling
